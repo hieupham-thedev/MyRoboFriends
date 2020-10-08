68e6e809fa7010ea2e7de4716f8cc7f34a3ae538
hieupham-thedreamer <hieu-pham.dev@outlook.com.vn>
2020-10-08 20:43:31 +0700

    Updates

---
+++ precache-manifest.14c13d55b41fb7ea2729c0d2eef76ead.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126e2238a7780fdba091dbc0c77819cc",
+    "url": "/MyRoboFriends/index.html"
+  },
+  {
+    "revision": "1af354b9c4eeca47ca0a",
+    "url": "/MyRoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "272f76c233b82826d70e",
+    "url": "/MyRoboFriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1af354b9c4eeca47ca0a",
+    "url": "/MyRoboFriends/static/js/2.e6604ef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyRoboFriends/static/js/2.e6604ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272f76c233b82826d70e",
+    "url": "/MyRoboFriends/static/js/main.790b7c4c.chunk.js"
+  },
+  {
+    "revision": "7dcebcea21207c9b8787",
+    "url": "/MyRoboFriends/static/js/runtime-main.ab75ab82.js"
+  }
+]);
